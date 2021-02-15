ca317f0de390d0675f994f1325750126b743d9ae
RyutoShimada <business.sub10110@gmail.com>
2021-02-15 19:40:53 +0900

    動く床を追加した

---
+++ Assets/Scripts/MoveFloorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFloorController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 1f;
+    [SerializeField] float m_width = 1f;
+    Vector3 m_startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_startPosition = this.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = m_startPosition + m_width * Mathf.Sin(m_speed * Time.time) * this.transform.forward;
+
+        //yは動かさない
+        Vector3 v3 = this.transform.position;
+        v3.y = 0;
+        this.transform.position = v3;
+    }
+}
