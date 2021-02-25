1a14f55151303809456a67a52dec3109766fecab
RyutoShimada <business.sub10110@gmail.com>
2021-02-25 11:36:48 +0900

    新しくSceneを追加

---
+++ Assets/Scripts/Story.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story : MonoBehaviour
+{
+	[SerializeField] string[] m_serihu = null;
+	IEnumerator Start()
+	{
+		Debug.Log("衝撃のファーストブリット");
+		yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+		yield return null;
+
+		Debug.Log("撃滅のセカンドブリット");
+		yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+		yield return null;
+
+		Debug.Log("抹殺のラストブリット");
+		List<string> serihu = new List<string>();
+		//serihu.ForEach(s => s.);
+	}
+}
