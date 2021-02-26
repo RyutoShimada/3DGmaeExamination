6e2b951aa5a86b3b97f5d04664f523f53c754d8e
RyutoShimada <business.sub10110@gmail.com>
2021-02-26 18:53:11 +0900

    アセット達

---
+++ Assets/Scripts/EndSceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class EndSceneManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_model = null;
+    [SerializeField] Transform m_target;
+    [SerializeField] float m_time = 3;
+    [SerializeField] float m_timing = 5f;
+    Animator m_anim;
+
+    private void Start()
+    {
+        m_anim = m_model.GetComponent<Animator>();
+        Invoke("ModelMove", m_timing);
+    }
+
+    public void ModelMove()
+    {
+        m_model.transform.DOMove(m_target.position, m_time);
+        m_anim.Play("Run");
+    }
+
+    public void StartLoad()
+    {
+        if (SceneManager.GetActiveScene().name == "EndScene")
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+}
